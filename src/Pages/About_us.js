874c4f1ce411d5632e5271e419e13c7f1033a6c5
Jakub <jakub.gelczynski@stud.pwsz.legnica.edu.pl>
2022-05-06 18:57:40 +0200

    porzadek i poprawki

---
+++ src/Pages/About_us.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import Navbar from '../Components/Navbar';
+import './About_us_style.css';
+
+function About_us() {
+    return (
+        <div className='custom-height'>
+            <Navbar />
+            <div className='container d-flex justify-content-center mt-50'>
+                <ul>
+                    <li><h4>Jakub Gelczyński - Web-app</h4></li>
+                    <li><h4>Jakub Engielski - Price-scrapper</h4></li>
+                    <li><h4>Patryk Biernacki - Desktop-app</h4></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default About_us
---
+++ src/Pages/Panel_rejestracji.js
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Panel_rejestracji_style.css'
+
+function Panel_rejestracji() {
+    return (
+        <div>
+            <div className="container w-25 reg-custom-div">
+
+                <h5 className='reg-h5'>Zarejestruj swoje konto:</h5>
+
+                <form className="needs-validation" action="/action_page.php" novalidate>
+
+                    <input type="text" id="user_name" className="form-control me-2" placeholder="Nazwa użytkownika" name="user_name" required/>
+                    <div className="invalid-feedback">
+                        Nic nie wpisano do tego pola!
+                    </div>
+                    <br/>
+
+                    <input type="password" id="password" className="form-control me-2" placeholder="Hasło" name="password" required/>
+                    <div className="invalid-feedback">
+                        Nic nie wpisano do tego pola!
+                    </div>
+                    <br/>
+
+                    <input type="password" id="password" className="form-control me-2" placeholder="Powtórz hasło" name="password" required/>
+                    <div className="invalid-feedback">
+                        Nic nie wpisano do tego pola!
+                    </div>
+                    <br/>
+
+                    <div className="form-check">
+                        <input className="form-check-input" type="checkbox" value="" id="invalidCheck" required/>
+                        <label className="form-check-label" for="invalidCheck">
+                            Oświadczam, że zapoznałem się z Regulaminem, akceptuję jego treść i zobowiązuję się przestrzegać jego postanowień.
+                        </label>
+                        <div className="invalid-feedback">
+                            Musisz wyrazić zgodę przed przesłaniem.
+                        </div>
+                    </div>
+                    
+                    <button className="btn btn-light w-100 reg-custom-btn" type="submit">Zarejestruj się</button>
+                </form>
+
+                <br/>
+                <p>Masz już konto? <Link onClick={() => {window.location.href="/panel_logowania"}}>Zaloguj się</Link></p>
+                
+            </div>
+
+        {/*
+        <script>
+              	// Example starter JavaScript for disabling form submissions if there are invalid fields
+				(function () {
+					'use strict'
+
+					// Fetch all the forms we want to apply custom Bootstrap validation styles to
+					var forms = document.querySelectorAll('.needs-validation')
+
+					// Loop over them and prevent submission
+					Array.prototype.slice.call(forms)
+						.forEach(function (form) {
+							form.addEventListener('submit', function (event) {
+								if (!form.checkValidity()) {
+									event.preventDefault()
+									event.stopPropagation()
+								}
+
+								form.classList.add('was-validated')
+							}, false)
+						})
+				})()
+        	</script>
+             */}
+        
+        </div>
+
+    )
+}
+
+export default Panel_rejestracji;
